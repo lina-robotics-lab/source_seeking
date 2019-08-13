5e0a816e1864b4435b0fac5abb8d113f51f3482e
victor <victor_l_qin@college.harvard.edu>
2019-08-13 10:10:49 -0400

    Editing and working with algorithm - need to solve some fundamental reading issues first

--- src/one_vector_RSS.py
+++ src/one_vector_RSS.py
@@ -12,7 +12,9 @@ import rospy
 from rospy.numpy_msg import numpy_msg
 
 from std_msgs.msg import Float32, Float32MultiArray
+from std_srvs.srv import Empty
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Twist
+from tf.transformations import euler_from_quaternion
 
 """Global Constants"""
 BURGER_MAX_LIN_VEL = 0.22
@@ -25,11 +27,15 @@ class one_vector_RSS():
 
         # Publisher and Subscribers
         self.move_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10);
-        #Subscribe to a position thing - either amcl or odom  make sure to numpy serialize
+        self.pose_subscriber = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.pose_callback)
         self.light_sub = rospy.Subscriber('sensor_readings', Float32MultiArray, self.light_callback)
 
+#        self.localization = rospy.ServiceProxy('global_localization', Empty, intialization)
+
         self.pose = Pose()
-        self.light = Float32()  #should just be the average
+        self.readings = deque([])
+        self.max = 0.0
+        self.light = 0.0  #should just be the average
         self.loc = np.array([0, 0])
 
 
@@ -38,42 +44,130 @@ class one_vector_RSS():
         self.states = deque([])
 
         # Track velocity, acceleration
-        self.vel = [0, 0]
-        self.acc = [0, 0]
+        self.vel = np.array([0.02, 0])
+        self.acc = np.array([0, 0])
 
     # Callbacks to get from subscribers
     def pose_callback(self, data):
         self.pose = data.pose.pose
-        self.loc = [self.pose.x, self.pose.y]
+        self.loc = [self.pose.position.x, self.pose.position.y]
 
     def light_callback(self, data):
-        avg = 0
-        for i in range(len(data.data)):
-            avg += data.data[i]
-        avg = avg/len(data.data)
-        self.light = avg
+        self.max = 0
+        temp = data.data
+        for i in temp:
+            if i > self.max:
+                self.max = i
+        print("max: " + str(self.max))
+#            print(temp)
+        if len(self.readings) >= 80:
+            for i in range(8):
+                self.readings.popleft()
+        self.readings.extend(temp)
 
+    def get_light(self):
+        r = rospy.Rate(1)
+        while not rospy.core.is_shutdown():
+            avg = 0
+            for i in range(len(self.readings)):
+                avg += self.readings[i]
+            avg = avg/len(self.readings)
+            print(avg)
+            r.sleep()
+#            self.light = avg
+
+    def initialization(self):
+        rospy.wait_for_service('/global_localization')
+        try:
+            initial = rospy.ServiceProxy('/global_localization', Empty)
+            print("tried")
+            return
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+        return
 
     # Calculate descent direction
     def calc_descent(self):
+        time.sleep(5)
 
-#        # Calculate descent direction
-#        sum = np.array([0, 0])
-#        gamma = 3
-#        factor = 0.1
-#        for i in range(len(self.states)):
-#            weight += (1/np.power(np.linalg.norm(self.states[i][1:2] - self.loc), gamma))
-#            sum += weight * (self.states[i][3] - self.light) * self.states[i][1:2]
-#        sum = sum/np.norm(sum)
-#        var_mag = 0.3
-#        sum += randn(1,2) * var_mag
-#        sum = factor * sum/np.norm(sum)
+        self.initialization()
+
+        twist = Twist()
+        twist.linear.x = self.vel[0]
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = self.vel[1]
+
+        print(twist)
+        self.move_pub.publish(twist)
 
 #        # Transmit the current velocity, then change it based on accel
-        r = rospy.Rate(10)
+        r = rospy.Rate(5)
         while not rospy.core.is_shutdown():
-            print(self.light)
+            # Add next state
+            self.get_light()
+            temp = np.append(self.loc,self.light)
+#            print(temp)
+            if len(self.states) >= 20:
+                self.states.popleft()
+            self.states.append(temp)
+
+
+            # Calculate descent direction
+            sum = np.array([0.0, 0.0])
+            weight = 0.0
+            gamma = -6
+            factor = 0.1
+            curState = self.states[-1]
+#            print("weight")
+            for i in range(0,len(self.states)-1):
+                dif = self.states[i][0:2] - curState[0:2]
+                dif_norm = np.linalg.norm(dif)
+                weight = (1/np.power(2, dif_norm)) - np.power(10,(gamma*dif_norm))
+                light_dif = weight * (self.states[i][2] - self.light) /(dif_norm)
+                vec = light_dif * dif
+                sum += vec
+            sum = sum/np.linalg.norm(sum)
+            if(np.any(np.isnan(sum))):
+                continue
+            var_mag = 0.3
+            sum += [random.gauss(0,1) * var_mag, random.gauss(0,1)* var_mag]
+            sum = np.array(factor * sum/np.linalg.norm(sum))
+#            print("sum")
+#            print(sum)
+
+            self.acc = np.matmul(np.array([[1.0, 0],[0,0.3]]),np.transpose(np.array(self.transmit(sum))))
+#            print(self.acc)
+            self.vel = self.vel + self.acc*0.2
+            if(np.abs(self.vel[0]) > 0.08):
+                print('check')
+                self.vel[0] = np.sign(self.vel[0])*0.08
+            if(np.abs(self.vel[1]) > 1.00):
+                self.vel[1] = np.sign(self.vel[1])*1.00
+
+            twist = Twist()
+            twist.linear.x = self.vel[0]
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = self.vel[1]
+
+            print(twist)
+            self.move_pub.publish(twist)
+
             r.sleep()
+
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+        self.move_pub.publish(twist)
 
         return
 
@@ -81,18 +175,20 @@ class one_vector_RSS():
     # Function to process vector into motion commands
 
     # Function to transmit the information - need transforms
-    def transmit(self):
+    def transmit(self, sum):
         qw = self.pose.orientation.w
         qx = self.pose.orientation.x
         qy = self.pose.orientation.y
         qz = self.pose.orientation.z
         (roll, pitch, yaw) = euler_from_quaternion((qx, qy, qz, qw))
 
-        bot_vel = np.array([[np.cos(yaw), -np.sin(yaw)],[np.sin(yaw), np.cos(yaw)]]) * self.vel
+        bot_vel = np.array([[np.cos(yaw), -np.sin(yaw)],[np.sin(yaw), np.cos(yaw)]])
+        bot_control = np.matmul(bot_vel, np.transpose(sum))
+        print(bot_control)
+        acc = np.dot(bot_control, [1,0])
+        ang_acc = np.dot(bot_control, [0, 1])
 
-
-
-        return
+        return [acc, ang_acc]
 
 
 """Intialize and run main function"""
@@ -105,7 +201,10 @@ if __name__=='__main__':
     logdebug("node[%s, %s] entering spin(), pid[%s]", rospy.core.get_caller_id(), rospy.core.get_node_uri(), os.getpid())
 
     try:
-        RSS.calc_descent()
+        time.sleep(1)
+#        RSS.initialization()
+        RSS.get_light()
+#        RSS.calc_descent()
     except KeyboardInterrupt:
         logdebug("keyboard interrupt, shutting down")
         rospy.core.signal_shutdown('keyboard interrupt')
